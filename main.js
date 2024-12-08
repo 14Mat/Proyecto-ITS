61416c708a81b9142053b8bf5a54359c67b84872
Matias Bravo <matiasbravoepet20@gmail.com>
2024-12-08 17:36:12 -0300

    mapa
    update mapa

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function iniciarMap(){
-    var coord = {lat:-38.9351044 ,lng: -68.1221581};
+    var coord = {lat:-38.9351044 ,lng:-68.1221581};
     var map = new google.maps.Map(document.getElementById('map'),{
       zoom: 10,
       center: coord
