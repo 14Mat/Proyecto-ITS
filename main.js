cea9c284c2f957da6b89883ffb08e139ef2db631
BAIVI2004 <102252187+BAIVI2004@users.noreply.github.com>
2024-12-10 11:49:14 -0300

    estructura
    estructurando el codigo

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function iniciarMap(){
+//la funcionalidad para que funcione el mapa
+function iniciarMap() {
     var coord = {lat:-38.9351044 ,lng:-68.1221581};
     var map = new google.maps.Map(document.getElementById('map'),{
       zoom: 10,
